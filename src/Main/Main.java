6572b39af54c9013cbbbeba99a2b1c1d95ae45f9
aymen <aymen.kouskoussi@etu.unistra.fr>
2022-10-29 18:00:15 +0200

    Initialisation fenetre avec swing

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame fenetre = new JFrame();
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setResizable(false);
+        fenetre.setTitle("Fils de Sparda");
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setVisible(true);
+
+    }
+}
